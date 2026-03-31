dd21046d7bfab83b607d4842531e45f34b3261fe
Deepanshu <deepanshuhead200@gmail.com>
2026-03-31 14:59:01 +0530

    Adding generic loader

---
+++ app/(protected)/account/loading.tsx
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-slate-950 via-slate-900 to-slate-950">
+      <div className="text-gray-300 text-xl">Loading...</div>
+    </div>
+  );
+}
